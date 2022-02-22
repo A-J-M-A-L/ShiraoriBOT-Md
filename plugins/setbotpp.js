4036c41161ddba217f6abc0ce3eed3182bea700a
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-02-22 07:01:17 +0530

    Update setbotpp.js

--- plugins/setbotpp.js
+++ plugins/setbotpp.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn }) => {
     let mime = (q.msg || q).mimetype || ''
     if (/image/.test(mime)) {
         let img = await q.download()
-        if (!img) throw `balas gambarnya!`
+        if (!img) throw `Reply to the picture!`
         conn.updateProfilePicture(bot, img)
-        m.reply('*Sukses*')
+        m.reply('*Success*')
     }
 }
 handler.help = ['setbotpp']
