7bdb6a410bd255eec1ff47f58a13be3cdf70bf55
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-02-22 12:56:00 +0530

    Update tebakkimia_hint.js

--- plugins/tebakkimia_hint.js
+++ plugins/tebakkimia_hint.js
@@ -5,6 +5,6 @@ let handler = async (m, { conn }) => {
     let json = conn.tebakkimia[id][1]
     m.reply('```' + json.name.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_') + '```')
 }
-handler.command = /^teki$/i
+handler.command = /^hint$/i
 handler.limit = true
 module.exports = handler
