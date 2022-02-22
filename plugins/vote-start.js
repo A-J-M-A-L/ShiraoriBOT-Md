038f0990de5eeb811ba6366c584a9b794851b8f4
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-02-22 12:50:55 +0530

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -2,16 +2,16 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
-        throw `_Masih ada vote di chat ini!_\n\n*${usedPrefix}hapusvote* - untuk menghapus vote`
+        throw `_There is still a vote in this chat!_\n\nuse *${usedPrefix}delvote* - To delete Vote.`
     }
-    m.reply(`Vote dimulai!\n\n*${usedPrefix}upvote* - untuk ya\n*${usedPrefix}devote* - untuk tidak\n*${usedPrefix}cekvote* - untuk mengecek vote\n*${usedPrefix}hapusvote* - untuk menghapus vote`)
+    m.reply(`Vote start!\n\n*${usedPrefix}upvote* - for yes\n*${usedPrefix}devote* - for not\n*${usedPrefix}checkvote* - to check vote\n*${usedPrefix}delvote* - To delete Vote.`)
     conn.vote[id] = [
         text,
         [],
         []
     ]
 }
-handler.help = ['mulaivote [alasan]']
+handler.help = ['startvote [alasan]']
 handler.tags = ['vote']
 handler.command = /^(start|mulai)vote$/i
 handler.limit = true
