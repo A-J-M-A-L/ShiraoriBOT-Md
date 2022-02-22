47deca625e4b203911abd760d043dc23ee7e1019
A-J-M-A-L <98047253+A-J-M-A-L@users.noreply.github.com>
2022-02-22 08:18:21 +0530

    Update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `Total Fitur saat ini: ${totalf}`,m)
+conn.reply(m.chat, `Total Features at this time: ${totalf}`,m)
 }
 
-handler.help = ['totalfitur']
+handler.help = ['totalfeature']
 handler.tags = ['info']
-handler.command = ['totalfitur']
+handler.command = ['totalfeature']
 module.exports = handler
